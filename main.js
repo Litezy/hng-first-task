ec996716aa9c800a66f50c7b4482b0c73b07478b
Bethel Nnadi <111425488+Litezy@users.noreply.github.com>
2023-09-11 07:19:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
-let time = document.querySelector('.time');
-let daytime = document.querySelector('.day');
-let profile =document.querySelector('section');
-window.onload= function (){
-    profile.classList.remove('off')
-    profile.classList.add('profile')
-}
-
-
-
-
+let daytime = document.querySelector(".day");
+let profile = document.querySelector("section");
+window.onload = function () {
+  profile.classList.remove("off");
+  profile.classList.add("profile");
+};
 
 const currentDate = new Date();
-
-const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const day = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const dayName = day[currentDate.getUTCDay()];
 
-const currentUTCTimeMillis = Date.now();
+const utcTimeString = ` ${dayName}`;
 
-const utcTimeString = 
-`Current Day of the Week: ${dayName}` ;
+function updateTime() {
+  const currentDate = new Date();
+  const timezone = { timezone: "Africa/Lagos" };
+  const local = currentDate.toLocaleTimeString("en-us", timezone);
+  const millisecond = currentDate.getTime();
+  document.querySelector(
+    ".time"
+  ).innerHTML = `Local Time: ${local} | UTC Time in (ms): ${millisecond} `;
+}
 
-
-
-time.innerHTML = utcTimeString
-daytime.innerHTML =`Current UTC Time in Milliseconds: ${currentUTCTimeMillis}`;
+daytime.innerHTML = `Day of the Week: ${dayName}`;
+updateTime();
+setInterval(updateTime, 1000);
